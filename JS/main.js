5b2b57c60c402188b2eb1086b6e0e7db898d0a4b
Kaizerpac <mendezelvis55@gmail.com>
2022-05-07 16:04:28 -0300

    comienzo del proyecto inver easy

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+document.getElementById("header").innerHTML=`
+
+`
+document.getElementById("header").innerHTML=`
+    <h1>Inver Easy</h1>
+        <nav>
+            <span class="izq">
+                <a href="index.html"><i class="fa-solid fa-fire fa-xl"></i></a>
+            </span>
+            <span class="der">
+                <a class="navsup" href="index.html">Principal</a>
+                <a class="navsup" href="contactos.html">contactos</a>
+                <a class="navsup" href="nosotros.html">nosotros</a>
+                <a class="navsup" href="servicios.html">Servicios</a>
+                <a class="navsup" href="productos.html">Productos</a>
+                <a class="navsup" href="sugerencias.html">Sugerencias</a>
+            </span>
+        </nav>
+        `
+document.getElementById("footer").innerHTML=`
+<nav>
+        <a href="https://www.twitter.com"><i class="fa-brands fa-twitter fa-lg "></i></a>
+        <a href="https://www.whatsapp.com"><i class="fa-brands fa-autoprefixer fa-lg"></i></a>
+        <a href="https://www.facebook.com"><i class="fa-brands fa-facebook fa-lg"></i></a>
+        </nav>
+        <p>Derechos Reservados 2022</p>
+`
+
