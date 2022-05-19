ff6db58e7bda7b2a15e446bd594caea032b2f244
KaizerP <mendezelvis55@gmail.com>
2022-05-18 22:50:01 -0300

    insercion de img header y article

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,11 @@
-document.getElementById("header").innerHTML=`
 
-`
 document.getElementById("header").innerHTML=`
     <h1>Inver Easy</h1>
         <nav>
-            <span class="izq">
+            <span1 class="izq">
                 <a href="index.html"><i class="fa-solid fa-fire fa-xl"></i></a>
-            </span>
-            <span class="der">
+            </span1>
+            <span2 class="der">
                 <a class="navsup" href="index.html">Principal</a>
                 <a class="navsup" href="contactos.html">contactos</a>
                 <a class="navsup" href="servicios.html">Servicios</a>
@@ -22,4 +20,6 @@ document.getElementById("footer").innerHTML=`
         <a href="https://www.whatsapp.com"><i class="fa-brands fa-autoprefixer fa-lg"></i></a>
         <a href="https://www.facebook.com"><i class="fa-brands fa-facebook fa-lg"></i></a>
         </nav>
-        <p text-aline: center>Derechos Reservados 2022
+        <span text-aline: center>Derechos Reservados 2022</span>
+        </nav>
+        `
